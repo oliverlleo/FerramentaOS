48c77588abfff89ef101e5965dabeb49beff9d63
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2025-06-12 15:49:59 -0300

    Fix Firebase config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,15 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, query, orderBy } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+// Authentication removed
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCB8ye34UIkBghExod4JlBV3oqge5oM2Ic",
     authDomain: "ferramentas-perfecta.firebaseapp.com",
     projectId: "ferramentas-perfecta",
-    storageBucket: "ferramentas-perfecta.firebasestorage.app",
+    databaseURL: "https://ferramentas-perfecta-default-rtdb.firebaseio.com",
+    storageBucket: "ferramentas-perfecta.appspot.com",
     messagingSenderId: "550722056346",
     appId: "1:550722056346:web:5076a97aca7fe276af6f23",
     measurementId: "G-8YKBJLSSL6"
@@ -17,7 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { db, auth, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, query, orderBy, signInWithEmailAndPassword, signOut, onAuthStateChanged };
+// Export Firestore utilities only
+export { db, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, query, orderBy };
 
